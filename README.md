75c02ca5670d9fbf180bf0fe422ec6077769813d
Georgy Bunin <bunin.co.il@gmail.com>
2020-06-16 22:54:49 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+1. fetch some icons from https://www.svgrepo.com/collection/outlined-landmarks/ to ./icons
+2. build custom font with icons (npm run build from ./icons folder)
+3. npx create-react-app icons-app
+4. use font with icons in your react app (I will copy it, but you can load font from CDN)
---
+++ icons-app/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>icons</h1>
+
+            <div className="preview">
+            <span className="inner">
+                <i className=" icon-big-ben-svgrepo-com"></i>
+            </span>
+                <br/>
+                <span className='label'>big-ben-svgrepo-com</span>
+            </div>
+
+
+            <div className="preview">
+            <span className="inner">
+                <i className=" icon-colloseum-svgrepo-com"></i>
+            </span>
+                <br/>
+                <span className='label'>colloseum-svgrepo-com</span>
+            </div>
+
+
+            <div className="preview">
+            <span className="inner">
+                <i className=" icon-egupt-sphinx-svgrepo-com"></i>
+            </span>
+                <br/>
+                <span className='label'>egupt-sphinx-svgrepo-com</span>
+            </div>
+
+
+            <div className="preview">
+            <span className="inner">
+                <i className=" icon-eiffel-tower-svgrepo-com"></i>
+            </span>
+                <br/>
+                <span className='label'>eiffel-tower-svgrepo-com</span>
+            </div>
+
+
+            <div className="preview">
+            <span className="inner">
+                <i className=" icon-leaning-tower-svgrepo-com"></i>
+            </span>
+                <br/>
+                <span className='label'>leaning-tower-svgrepo-com</span>
+            </div>
+
+
+            <div className="preview">
+            <span className="inner">
+                <i className=" icon-opera-house-svgrepo-com"></i>
+            </span>
+                <br/>
+                <span className='label'>opera-house-svgrepo-com</span>
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
